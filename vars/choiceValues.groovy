d9b3b5ff903249101df1f16b344068eb15305dd0
NJ <jainnitin2411@users.noreply.github.com>
2024-02-07 15:00:04 +0530

    Update choiceValues.groovy
    Removed extra code

--- vars/choiceValues.groovy
+++ vars/choiceValues.groovy
@@ -1,9 +1,9 @@
 def call() {
   def full_string = "Git1\nGit2"
-  def arr = full_string.split("\n")
-  for (i in arr) {
-    println "now got ${i}"
-  }
+//  def arr = full_string.split("\n")
+// for (i in arr) {
+//    println "now got ${i}"
+//  }
   println "${full_string}"
 
   return full_string
