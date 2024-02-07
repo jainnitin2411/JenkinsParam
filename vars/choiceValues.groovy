5d5e964f345add36decb05897602300e3fac4cb0
NJ <jainnitin2411@users.noreply.github.com>
2024-02-07 14:55:23 +0530

    Update choiceValues.groovy
    return single string value

--- vars/choiceValues.groovy
+++ vars/choiceValues.groovy
@@ -1,9 +1,11 @@
 def call() {
-  def full_string = "Git1 Git2"
+  def full_string = "Git1 
+  Git2"
   def arr = full_string.split(" ")
   for (i in arr) {
     println "now got ${i}"
   }
+  println "${full_string}"
 
-  return arr
+  return full_string
 }
