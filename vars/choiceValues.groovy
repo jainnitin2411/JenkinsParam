9eccd45e2910e6e00fdd378e338733c074e7ac0b
NJ <jainnitin2411@users.noreply.github.com>
2024-02-07 15:21:09 +0530

    Update choiceValues.groovy
    commented extra code

--- vars/choiceValues.groovy
+++ vars/choiceValues.groovy
@@ -1,11 +1,9 @@
 def call() {
-  def listVal = ['Test1', 'Test2']
-  def full_string = "VM\nDesktop"
-//  def arr = full_string.split("\n")
-// for (i in arr) {
-//    println "now got ${i}"
-//  }
-  println "${full_string}"
-
+  def listVal = ['VM', 'Desktop']
   return listVal
+
+  // OR
+  // def full_string = "VM\nDesktop"
+  // println "${full_string}"
+  // return listVal
 }
