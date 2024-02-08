16e5125d5c1b6e596ffa4b92f8f2044a8bc01f74
NJ <jainnitin2411@users.noreply.github.com>
2024-02-08 14:57:22 +0530

    Update activeChoiceParam.groovy
    change quotation type

--- vars/activeChoiceParam.groovy
+++ vars/activeChoiceParam.groovy
@@ -1,7 +1,7 @@
 def call() {
   //return 'file1.txt\nfile2.txt\nfile3.txt\nfile4.txt'
   //println "full_string"
-   def fruitTypes = ["APPLE", "BANANA", "ORANGE"]
+   def fruitTypes = ['APPLE', 'BANANA', 'ORANGE']
  
    return fruitTypes
   
